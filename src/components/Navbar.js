f642a9567cd2b15aa2db5ba181651a8c081c4670
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2023-01-26 11:13:43 -0300

    Navbar and logo advance

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,8 @@ function Navbar() {
   return (
     <nav>
       <div className="navbar-container">
-        
+        <p className='la'>ES</p>
+        <p className='la'>EN</p>
       </div>
     </nav>
   )
