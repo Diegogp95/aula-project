7def7708efa29810c48022837184d65f9dae5a36
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2023-02-07 19:55:45 -0300

    cleaner dropdown with ul

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,58 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav>
-      <div className="navbar-container">
-        <a href="https://usm.cl/">
-          <p className='navbar-item'>USM.cl</p>
-        </a>
-        <div className="rightside-dropdown">
-          <p className="navbar-item">Recursos Aula</p>
-          <p className="navbar-item">Sitios de interés</p>
+      <div className="navbar">
+        <div className="group">
+          <ul>
+            <li>
+              <a href="https://usm.cl/">
+                USM.cl
+                </a>
+            </li>
+          </ul>
+        </div>
+        <div className="group">
+          <ul>
+            <li>
+              <a href="#">
+                Recursos Aula
+              </a>
+              <ul>
+                <li>
+                  <a href="https://educacionadistancia.usm.cl/recursos-aula-profesores/">
+                    Recursos Aula Profesores
+                  </a>
+                </li>
+                <li>
+                  <a href="https://educacionadistancia.usm.cl/recursos-aula-estudiantes/">
+                    Recursos Aula Estudiantes
+                  </a>
+                </li>
+                <li>
+                  <a href="https://educacionadistancia.usm.cl/recursos-zoom/">
+                    Recursos Zoom
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">
+                Sitios de interés
+              </a>
+              <ul className='rightDrop'>
+                <li>
+                  <a href="https://usm.cl/sitios-academicos/">
+                    Sitios Académicos
+                  </a>
+                </li>
+                <li>
+                  <a href="https://usm.cl/sitios-administrativos/">
+                    Sitios Administrativos
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
