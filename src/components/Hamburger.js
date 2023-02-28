335668f8ca541425b27f1494657bc4005f0c4af6
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2023-02-28 13:58:06 -0300

    replaced hamburger btn animation for hover

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Hamburger.css';
 
-function Hamburger({ isDisplayed, toggleDisplay }) {
+function Hamburger({ toggleDisplay }) {
 
   return (
     <div className='ham-btn' onClick={toggleDisplay}>
       <div className="ham-icon">
-        <div className={`ham-bar ${isDisplayed ? 'test' : ''}`} id='top-bar'></div>
-        <div className={`ham-bar ${isDisplayed ? 'test' : ''}`} id='mid-bar'></div>
-        <div className={`ham-bar ${isDisplayed ? 'test' : ''}`} id='bot-bar'></div>
+        <div className={`ham-bar`} id='top-bar'></div>
+        <div className={`ham-bar`} id='mid-bar'></div>
+        <div className={`ham-bar`} id='bot-bar'></div>
       </div>
     </div>
   )
