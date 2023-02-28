5261fa34bba07f90f1734a40e5902141c4ca9aa0
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2023-02-28 20:56:05 -0300

    footer width limited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,14 @@ import usm from './img/usm_cl.svg';
 
 function Footer() {
   return (
-    <div className="Footer">
-        <FooterText />
-        <a href="https://usm.cl/" target="_blank" rel="noreferrer" >
-          <img src={usm} alt="" className='uhome'/>
-        </a>
+    <div className="backgroundwrap">
+      <div className="Footer container limit-width">
+          <FooterText />
+          <a href="https://usm.cl/" target="_blank" rel="noreferrer" >
+            <img src={usm} alt="" className='uhome'/>
+          </a>
+      </div>
     </div>
   )
 }
-
 export default Footer
